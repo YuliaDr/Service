d74931134d0bab9d76d072b5b5b2d2614fcd22a9
YuliaDr <strekozka_ulya@mail.ru>
2019-01-12 16:38:38 +0900

    FirstCommit

---
+++ app/src/main/java/com/example/user/servis/MyService.java
@@ -0,0 +1,66 @@
+package com.example.user.servis;
+
+
+import android.app.Notification;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+import android.support.v4.app.NotificationCompat;
+import android.widget.Toast;
+
+public class MyService extends Service {
+
+    private MediaPlayer mPlayer;
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Toast.makeText(this, "Служба создана",
+                Toast.LENGTH_SHORT).show();
+        mPlayer = MediaPlayer.create(this, R.raw.let);
+        mPlayer.setLooping(false);
+    }
+
+    private void runAsForeground(){
+        Intent notificationIntent = new Intent(this, MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0,
+                notificationIntent, 0);
+
+        Notification notification = new Notification.Builder(this)
+                .setContentText("Hi")
+                .setContentIntent(pendingIntent).build();
+
+        startForeground(1337, notification);
+
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Toast.makeText(this, "Служба запущена",
+                Toast.LENGTH_SHORT).show();
+        mPlayer.start();
+        runAsForeground();
+        //stopForeground(false);
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Toast.makeText(this, "Служба остановлена",
+                Toast.LENGTH_SHORT).show();
+        mPlayer.stop();
+
+    }
+
+
+}
